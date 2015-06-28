a3192554c2a0815c9204bb5223ef37483eb20d10
Mark Horton <mark@nostromo.net>
2015-06-28 19:56:26 -0400

    adding javah.sh

---
+++ javah.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+TARGET_DIR="$HOME/ClionProjects/libc_jni"
+
+gradle clean build
+cd build/classes/main
+
+javah -d $TARGET_DIR net.nostromo.libc.file.LibcFile
+javah -d $TARGET_DIR net.nostromo.libc.io.LibcIo
+javah -d $TARGET_DIR net.nostromo.libc.network.LibcNetwork
+javah -d $TARGET_DIR net.nostromo.libc.system.LibcSystem
+
